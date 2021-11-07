15b263095c662934adb91e3fa601ea922d418ea4
Julien Travers <julien.travers@epita.fr>
2021-11-07 18:42:47 +0100

    vas y bg, tape le fond du cors

--- assets/js/find.js
+++ assets/js/find.js
@@ -1,58 +1,98 @@
-const table = document.getElementById("table-content");
+var bodyObj =
+    {
+        "filters" : 
+          {
+              "type_local": "Maison",
+              "code_postal": 75016
+          }
+      }
 
-const columns = ["price", "address", "city", "zip"];
+fetch('http://localhost:3000/home/averagePrice/apartment', {
+        method: 'POST',
+        body: JSON.stringify(bodyObj),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+            'Access-Control-Allow-Origin': '*',
+            'Accept-Language': 'en-us,en;q=0.5',
+            'Accept-Encoding': 'gzip,deflate',
+            'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+            'Connection': 'keep-alive'
+        }
+    })
+    .then(response => response.json())
+    .then(json => {
+        console.log(json);
+    });
 
-//Begin API Call
 
-var xhr = new XMLHttpRequest();
-xhr.onload = function () {
+// const table = document.getElementById("table-content");
 
-	if (xhr.status >= 200 && xhr.status < 300) {
+// const columns = ["price", "address", "city", "zip"];
 
-      const res = JSON.parse(xhr.response);
-      console.log(res);
+// //Begin API Call
+
+// var xhr = new XMLHttpRequest();
+
+// xhr.onload = function () {
+
+// 	if (xhr.status >= 200 && xhr.status < 300) {
+
+//       const res = JSON.parse(xhr.response);
+//       console.log(res);
     
-	} else {
-      console.log('The request failed!');
-	}
-};
+// 	} else {
+//       console.log('The request failed!');
+// 	}
+// };
 
-xhr.open('GET', 'https://localhost:6666/home/one');
-xhr.send()
-//End API Call
+// xhr.open('POST', 'http://localhost:3000/home/averagePrice/apartment');
+// xhr.setRequestHeader('Content-Type', 'application/json');
+// var body =
+//     {
+//         "filters" : 
+//           {
+//               "type_local": "Maison",
+//               "code_postal": 75016
+//           }
+//       }
 
-const data = [
-    {
-        price: "270 000€",
-        address: "17 rue du bourg",
-        city: "Juziers",
-        zip: "78820"
-    },
-    {
-        price: "500 000€",
-        address: "48 rue de l'église",
-        city: "Juziers",
-        zip: "78820"
-    }
-]
+// var bodyJson = JSON.stringify(body)
+// console.log(bodyJson);
+// xhr.send(bodyJson);
+// //End API Call
 
-for (const i in data) {
-    const elt = data[i];
-    const tr = document.createElement('tr')
-    for (const c in columns) {
-        const column = columns[c]
-        const td = document.createElement('td');
-        td.innerHTML = elt[column];
-        tr.appendChild(td)
-    }
-    table.appendChild(tr);
-}
+// const data = [
+//     {
+//         price: "270 000€",
+//         address: "17 rue du bourg",
+//         city: "Juziers",
+//         zip: "78820"
+//     },
+//     {
+//         price: "500 000€",
+//         address: "48 rue de l'église",
+//         city: "Juziers",
+//         zip: "78820"
+//     }
+// ]
 
-const search = () => {
-    const zip = document.getElementById("zip").value;
-    const type = document.getElementById("type").value;
-    const surface = document.getElementById("surface").value;
-    const rooms = document.getElementById("rooms").value;
-    const price = document.getElementById("price").value;
-    //ToDo : API call with parameters
-}
+// for (const i in data) {
+//     const elt = data[i];
+//     const tr = document.createElement('tr')
+//     for (const c in columns) {
+//         const column = columns[c]
+//         const td = document.createElement('td');
+//         td.innerHTML = elt[column];
+//         tr.appendChild(td)
+//     }
+//     table.appendChild(tr);
+// }
+
+// const search = () => {
+//     const zip = document.getElementById("zip").value;
+//     const type = document.getElementById("type").value;
+//     const surface = document.getElementById("surface").value;
+//     const rooms = document.getElementById("rooms").value;
+//     const price = document.getElementById("price").value;
+//     //ToDo : API call with parameters
+// }
